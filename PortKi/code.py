7e6a49e36148c553d1e532e64784fcecb08ae918
John Gallaugher <john.gallaugher@bc.edu>
2019-08-03 13:53:51 -0400

    code.py now checks to see if there is a portki-files directory. If not, it creates one. If there is one, it deletes all of the files inside before wget downloading the most current files.

--- PortKi/code.py
+++ PortKi/code.py
@@ -22,9 +22,33 @@ import os
 import digitalio
 import busio
 
+# code currently removes the portki-files directory & its contents when
+# rebooted. At some point I may want to only remove updated files, but this works.
 # DIRECTORY = "/sd/" # sd card
-DIRECTORY = "/portki-files/"
-# os.mkdir(DIRECTORY)
+DIRECTORY_NAME = "portki-files"
+DIRECTORY = "/"+DIRECTORY_NAME+"/"
+
+try:
+    result = os.rmdir("/"+DIRECTORY_NAME)
+    print("Contents of", DIRECTORY, "are:")
+    directory_files = os.listdir("portki-files")
+    for file in directory_files:
+        print(file)
+        os.remove(DIRECTORY+file)
+except OSError as e:
+    print("Error:", OSError, e)
+    print("No directory named:", DIRECTORY_NAME, " Will try to create one.")
+
+try:
+    result = os.mkdir("/"+DIRECTORY_NAME)
+except OSError:
+    print("Directory portki-files already exists - no need to make a new one")
+
+
+"""
+    tag = subprocess.run('mkdir /portki-files/', shell=True)
+    print(tag)
+    """
 
 DATA_SOURCE = "https://io.adafruit.com/api/v2/gallaugher/feeds/portki/data/"
 DATA_LOCATION = [0, "value"]
@@ -58,11 +82,11 @@ class Screen:
 def read_json_into_screens():
     for i in range(len(screens_list)):
         pageID = screens_list[i]["pageID"]
-        print("pageID =", pageID)
+        # print("pageID =", pageID)
         screenURL = screens_list[i]["screenURL"]
-        print("screenURL =", screens_list[i]["screenURL"])
+        # print("screenURL =", screens_list[i]["screenURL"])
         num_of_buttons = len(screens_list[i]["buttons"])
-        print("This page has", num_of_buttons, "buttons")
+        # print("This page has", num_of_buttons, "buttons")
         buttons = []
         for button_index in range(num_of_buttons):
             buttonText = screens_list[i]["buttons"][button_index]["text"]
@@ -75,26 +99,41 @@ def read_json_into_screens():
             buttons.append(button)
         screen = Screen(pageID, buttons, screenURL)
         screens.append(screen)
-    
+
+"""
     for screen in screens:
-        print(screen.pageID)
-        print(screen.screenURL)
-        print("This screen has", len(screen.buttons), "buttons")
-        for index in range(len(screen.buttons)):
-            print("  Button", index, "text =", screen.buttons[index].buttonText)
-            print("  Button", index, "buttonDestination =", screen.buttons[index].buttonDestination)
-            print("  Button", index, "has coordinates:", screen.buttons[index].x, screen.buttons[index].y, screen.buttons[index].width, screen.buttons[index].height)
+    print(screen.pageID)
+    print(screen.screenURL)
+    print("This screen has", len(screen.buttons), "buttons")
+    for index in range(len(screen.buttons)):
+    print("  Button", index, "text =", screen.buttons[index].buttonText)
+    print("  Button", index, "buttonDestination =", screen.buttons[index].buttonDestination)
+    print("  Button", index, "has coordinates:", screen.buttons[index].x, screen.buttons[index].y, screen.buttons[index].width, screen.buttons[index].height)
+    """
+
+def flush(self):
+    """Writes changes to flash"""
+        if self.dirty:
+            with open(self.filename, "wt") as file:
+                file.write(json.dumps(self.data))
+                file.flush()
+            os.sync()
+            self.dirty = False
 
 try:
     response = pyportal.fetch()
-    print("%%%% JSON RETRIEVED %%%%")
-    print("??? Len of response: ", len(response))
-    print("*** PRINTING response:\n", response, "\n")
-    print("*** response is of type:\n", type(response), "\n")
+    """
+        print("%%%% JSON RETRIEVED %%%%")
+        print("??? Len of response: ", len(response))
+        print("*** PRINTING response:\n", response, "\n")
+        print("*** response is of type:\n", type(response), "\n")
+        """
     convertedJson = json.loads(response)
-    print("*** convertedJson is of type:\n", type(convertedJson), "\n")
-    print("*** PRINTING convertedJson:\n", convertedJson, "\n")
-    print(convertedJson[0]["pageID"])
+    """
+        print("*** convertedJson is of type:\n", type(convertedJson), "\n")
+        print("*** PRINTING convertedJson:\n", convertedJson, "\n")
+        print(convertedJson[0]["pageID"])
+        """
     screens_json = convertedJson
     
     screens_list = screens_json
@@ -110,10 +149,16 @@ current_pageID = "Home"
 for screen in screens:
     image_url = screen.screenURL
     fileName = screen.pageID+".bmp"
-    print("image_url =", image_url)
-    print("screen.screenURL = ", screen.screenURL)
-    pyportal.wget(pyportal.image_converter_url(image_url,320, 240,color_depth=16), DIRECTORY+fileName, chunk_size=12000)
+    # print("image_url =", image_url)
+    # print("screen.screenURL =", screen.screenURL)
+    # print("Saving at:", DIRECTORY+fileName)
+    try:
+        pyportal.wget(pyportal.image_converter_url(image_url,320, 240,color_depth=16), DIRECTORY+fileName, chunk_size=12000)
+    except OSError as e:
+        print("<><><><> SOME ERROR OCCURRED! -", OSError, e)
+        print("Please restart PyPortal by pressing the Reset button on the back of the device")
 
+print("Setting background to:", DIRECTORY+"Home.bmp")
 pyportal.set_background(DIRECTORY+"Home.bmp")
 
 while True:
@@ -149,7 +194,14 @@ while True:
                         break_outer_loop = True
                         current_pageID = screen.buttons[index].buttonDestination
                         print("Time to load screen", current_pageID)
-                        pyportal.set_background(DIRECTORY+current_pageID+".bmp")
+                        try:
+                            pyportal.set_background(DIRECTORY+current_pageID+".bmp")
+                        except OSError as e:
+                            # TODO: print this error to PyPortal screen.
+                            print("*** Error getting image from", DIRECTORY+current_pageID)
+                            print("*** Please turn device over and press the Reset button")
+                            print("*** If this doesn't work, open the PortKi app and select:")
+                            print("***     Update PyPortal   ***")
                         break
             if break_outer_loop:
                 break
